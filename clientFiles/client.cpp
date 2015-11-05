dfd9dbaaf948c940ce07602a74c919d4a1e0d8df
Brian Crosser <crosserb@seawolf.sonoma.edu>
2015-11-05 11:37:48 -0800

    Stage 1 Complete

--- clientFiles/client.cpp
+++ clientFiles/client.cpp
@@ -11,10 +11,12 @@
 #include <string.h>
 #include <string>
 #include <iostream>
+#include <thread>
 
 #define SERVER_PORT_ID 8060
 #define CLIENT_PORT_ID 8061
 
+#define MAXLINE 256
 #define MAXSIZE 512
 
 #define ACK                   2
@@ -34,19 +36,19 @@ using namespace std;
 int readn(int sd,char *ptr,int size);
 int writen(int sd,char *ptr,int size);
 void getUserInput(string & userCommand);
+void receiveCommand(int newsd);
+void giveCommand(int sockid);
 
 int main(int argc,char *argv[])
 {
-    int sockid, newsockid,getcommand,ack,msg,msg_2,len;
-    string userCommand;
+	int sockid, newsd;
     struct sockaddr_in my_addr, server_addr; 
+    
     if(argc != 2) {
         printf("error: usage : %s IP-dotted-notation\n", argv[0]); 
         exit(0);
     }
- 
-    getUserInput(userCommand);
-    len = userCommand.size();
+     
     printf("client: creating socket\n");
     if ((sockid = socket(AF_INET,SOCK_STREAM,0)) < 0)  { 
         printf("client: socket error : %s\n",  strerror( errno )); 
@@ -62,71 +64,158 @@ int main(int argc,char *argv[])
         printf("client: bind  error :%s\n",  strerror( errno )); 
         exit(0);
     }
-                                             
-    printf("client: starting connect\n");
+
+	printf("client: starting connect\n");
     bzero((char *) &server_addr,sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = inet_addr(argv[1]);
     server_addr.sin_port = htons(SERVER_PORT_ID);
-    if (connect(sockid ,(struct sockaddr *) &server_addr,
+	if (connect(sockid ,(struct sockaddr *) &server_addr,
                 sizeof(server_addr)) < 0) {
         printf("client: connect  error :%s\n",  strerror( errno )); 
         exit(0);
     }
+    cout << "Before thread" << endl;
+    //thread giveCmd (giveCommand, sockid);
+    giveCommand(sockid);
+    cout << "After thread" << endl;
+	//giveCmd.detach();
 
-    /* Once we are here, we've got a connection to the server */
-
-    /* tell server that we want to get a file */
-    getcommand = htons(REQUESTCOMMAND);
-    printf("client: sending command request to ftp server\n");
-    if((writen(sockid,(char *)&getcommand,sizeof(getcommand))) < 0) {
-        printf("client: write  error :%s\n",  strerror( errno )); 
-        exit(0);
-    } 
-
-    /* want for go-ahead from server */
-    msg = 0;  
-    if((readn(sockid,(char *)&msg,sizeof(msg)))< 0) {
-        printf("client: read  error :%s\n",  strerror( errno )); 
-        exit(0); 
-    }
-    msg = ntohs(msg);   
-    if (msg==COMMANDNOTSUPPORTED) {
-        printf("client: server refused command. goodbye\n");
-        exit(0);
-    } else
-        printf("client: server replied %d, command supported\n", msg);
-
-    /* send file name to server */
-    printf("client: sending command\n");
-    if ((writen(sockid, (char *)userCommand.c_str(),len))< 0) {
-        printf("client: write  error :%s\n",  strerror( errno )); 
+	/*
+    while( true ) {
+        printf("server: starting accept\n");
+	    if (connect(sockid ,(struct sockaddr *) &server_addr,
+                sizeof(server_addr)) < 0) {
+        printf("client: connect  error :%s\n",  strerror( errno )); 
         exit(0);
     }
-    /* see if server replied that file name is OK */
-    msg_2 = 0;
-    if (readn(sockid, (char *)&msg_2, sizeof(msg_2)) < 0) {
-        printf("client: read  error :%s\n",  strerror( errno )); 
-        exit(0); 
-    }   
-    msg_2 = ntohs(msg_2);
-    if (msg_2 == BADCOMMAND) {       
-        printf("client: server reported bad command. goodbye.\n");
-        exit(0);
-    } else
-        printf("client: server replied %d, filename OK\n",msg_2);
-
-
-    /*File transfer ends. client terminates after  closing all its files
-      and sockets*/ 
-    printf("client: Command TRANSFER COMPLETE\n");
-    close(sockid);
+                
+        thread receiveCmd (receiveCommand, newsd);
+        receiveCmd.detach();
+    }
+	*/
 }            
+
+void receiveCommand(int sockid)
+{
+	int msg_ok,fail,req;
+    int tmp;
+    char command[MAXLINE], commandLen = 0;
+    
+    
+    /* Start servicing the client */ 
   
-/* Due to the fact that buffer limits in kernel for the socket may be 
-   reached, it is possible that read and write may return a positive value
-   less than the number requested. Hence we call the two procedures
-   below to take care of such exigencies */
+    /* get command code from client.*/
+    /* only one supported command: 100 -  get a file */
+    req = 0;
+    if((readn(sockid,(char *)&req,sizeof(req))) < 0) {
+        perror( "Read error" );
+        exit(0);
+    }
+    req = ntohs(req);
+    printf("server: client request code is: %d\n",req);
+    if (req!=REQUESTCOMMAND) {
+        printf("server: unsupported operation. goodbye\n");
+        /* reply to client: command not OK  (code: 150) */
+        msg_ok = COMMANDNOTSUPPORTED; 
+        msg_ok = htons(msg_ok);
+        if((writen(sockid,(char *)&msg_ok,sizeof(msg_ok))) < 0) {
+            perror( "Write error" );
+            exit(0);
+        }
+        exit(0);
+    }
+
+    /* reply to client: command OK  (code: 160) */
+    msg_ok = COMMANDSUPPORTED; 
+    msg_ok = htons(msg_ok);
+    if( writen(sockid,(char *)&msg_ok,sizeof(msg_ok))  < 0) {
+        perror( "Write error" );
+        exit(0);
+    }
+  
+    fail = COMMANDOK;
+    if((commandLen = read(sockid,command,MAXLINE)) < 0) {
+        perror("Command read error" );
+        fail = BADCOMMAND ;
+    }
+    command[ commandLen ] = '\0';
+   
+    tmp = htons(fail);
+    if((writen(sockid,(char *)&tmp,sizeof(tmp))) < 0) {
+        perror("Write error" );
+        exit(0);   
+    }
+    if(fail == BADCOMMAND) {
+        printf("server cant do command");
+        close(sockid);
+        exit(0);
+    }
+    printf("server: command is %s\n",command);
+  
+    printf("server: Command TRANSFER COMPLETE on socket %d\n",sockid);
+    close(sockid);
+	
+}
+
+void giveCommand(int sockid)
+{
+    cout << "Check Point Reached!" << endl;
+	string userCommand;
+	
+	getUserInput(userCommand);
+    
+	while(userCommand != "shutdown"){
+        int getcommand,msg,msg_2,len;
+        len = userCommand.size();
+
+	    getcommand = htons(REQUESTCOMMAND);
+        printf("client: sending command request to ftp server\n");
+        if((writen(sockid,(char *)&getcommand,sizeof(getcommand))) < 0) {
+            printf("client: write  error :%s\n",  strerror( errno )); 
+            exit(0);
+        } 
+
+        /* want for go-ahead from server */
+        msg = 0;  
+        if((readn(sockid,(char *)&msg,sizeof(msg)))< 0) {
+            printf("client: read  error :%s\n",  strerror( errno )); 
+         exit(0); 
+        }
+        msg = ntohs(msg);   
+        if (msg==COMMANDNOTSUPPORTED) {
+            printf("client: server refused command. goodbye\n");
+            exit(0);
+        } else
+            printf("client: server replied %d, command supported\n", msg);
+
+        /* send file name to server */
+        printf("client: sending command\n");
+        if ((writen(sockid, (char *)userCommand.c_str(),len))< 0) {
+            printf("client: write  error :%s\n",  strerror( errno )); 
+            exit(0);
+        }
+        /* see if server replied that file name is OK */
+        msg_2 = 0;
+        if (readn(sockid, (char *)&msg_2, sizeof(msg_2)) < 0) {
+            printf("client: read  error :%s\n",  strerror( errno )); 
+            exit(0); 
+        }   
+        msg_2 = ntohs(msg_2);
+        if (msg_2 == BADCOMMAND) {       
+            printf("client: server reported bad command. goodbye.\n");
+            exit(0);
+        } else
+            printf("client: server replied %d, filename OK\n",msg_2);
+    
+
+        /*File transfer ends. client terminates after  closing all its files
+          and sockets*/ 
+        printf("client: Command TRANSFER COMPLETE\n");
+        getUserInput(userCommand);
+    }
+    //close(sockid);
+}
 
 int readn(int sd, char *ptr, int size)
 {
@@ -159,5 +248,5 @@ int writen(int sd,char *ptr,int size)
 void getUserInput(string & userCommand)
 {
     cout << "~: ";
-    cin >> userCommand;
+    getline(cin, userCommand);
 }
--- serverFiles/server.cpp
+++ serverFiles/server.cpp
@@ -1,14 +1,3 @@
-
-/* This is the server for a very simple file transfer
-   service.  This is a "concurrent server" that can
-   handle requests from multiple simultaneous clients.
-   For each client:
-   - get file name and check if it exists
-   - send size of file to client
-   - send file to client, a block at a time
-   - close connection with client
-*/
-
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -19,7 +8,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <strings.h>
+#include <string.h>
+#include <string>
 #include <thread>
+#include <iostream>
 
 #define MY_PORT_ID 8060
 #define MAXLINE 256
@@ -36,16 +28,18 @@
 #define FILENAMEOK            400
 #define COMMANDOK			  410
 
+using namespace std;
+
 int writen(int sd,char *ptr,int size);
 int readn(int sd,char *ptr,int size);
-void doftp(int newsd);
-void doctp(int newsd);
+void getUserInput(string & userCommand);
+void giveCommand(int newsd);
+void receiveCommand(int newsd);
 
 int main()  {
-    int sockid, newsd, pid; 
-    struct sockaddr_in my_addr, client_addr;   
-    socklen_t clilen = sizeof( client_addr );
-
+    int sockid, newsd; 
+    struct sockaddr_in my_addr, client_addr;
+    socklen_t clilen = sizeof(client_addr);
     printf("server: creating socket\n");
     if ((sockid = socket(AF_INET,SOCK_STREAM,0)) < 0) {
         printf("server: socket error : %d\n", errno); 
@@ -66,209 +60,28 @@ int main()  {
         perror( "Listen error" );
         exit(0);
     }                                        
-    for( ;; ) { 
-        /* Accept a connection and then create a child to do the work */
-        /* loop back and wait for another connection                  */
+    
+    //thread giveCmd (giveCommand, newsd);
 
+    while( true ) {
         printf("server: starting accept\n");
-        if ((newsd = accept(sockid ,(struct sockaddr *) &client_addr, &clilen)) < 0) {
-            perror( "Failed to accept" );
-            exit(0); 
-        }
-        printf("server: return from accept, socket for this ftp: %d\n", newsd);
-        if ( (pid=fork()) == 0) {
-            /* Child proc starts here. it will do actual file transfer */
-            close(sockid);   /* child shouldn't do an accept */
-            //doftp(newsd);
-			doctp(newsd);
-            close (newsd);
-            exit(0);         /* child all done with work */
-        }
-        /* Parent continues below here */
-        close(newsd);    /* parent all done with client, only child */
-    }                  /* will communicate with that client from now on */
+	    if ((newsd = accept(sockid ,(struct sockaddr *) &client_addr, &clilen)) < 0) {
+    	    perror( "Failed to accept" );
+	        exit(0); 
+	    }
+                
+        thread receiveCmd (receiveCommand, newsd);
+        receiveCmd.detach();
+    }
+		
 }   
      
 
-/* Child procedure, which actually does the file transfer */
-void doftp(int newsd)
-{       
-    int i,fsize,fd,msg_ok,fail,fail1,req,c,ack;
-    int no_read ,num_blks , num_blks1,num_last_blk,num_last_blk1,tmp;
-    char fname[MAXLINE], fnameLen = 0;
-    char out_buf[MAXSIZE];
-    FILE *fp;
-      
-    no_read = 0;
-    num_blks = 0;
-    num_last_blk = 0; 
-
-    
-    /* Start servicing the client */ 
-  
-    /* get command code from client.*/
-    /* only one supported command: 100 -  get a file */
-    req = 0;
-    if((readn(newsd,(char *)&req,sizeof(req))) < 0) {
-        perror( "Read error" );
-        exit(0);
-    }
-    req = ntohs(req);
-    printf("server: client request code is: %d\n",req);
-    if (req!=REQUESTFILE) {
-        printf("server: unsupported operation. goodbye\n");
-        /* reply to client: command not OK  (code: 150) */
-        msg_ok = COMMANDNOTSUPPORTED; 
-        msg_ok = htons(msg_ok);
-        if((writen(newsd,(char *)&msg_ok,sizeof(msg_ok))) < 0) {
-            perror( "Write error" );
-            exit(0);
-        }
-        exit(0);
-    }
-
-    /* reply to client: command OK  (code: 160) */
-    msg_ok = COMMANDSUPPORTED; 
-    msg_ok = htons(msg_ok);
-    if( writen(newsd,(char *)&msg_ok,sizeof(msg_ok))  < 0) {
-        perror( "Write error" );
-        exit(0);
-    }
-  
-    fail = FILENAMEOK;
-    if((fnameLen = read(newsd,fname,MAXLINE)) < 0) {
-        perror("Filename read error" );
-        fail = BADFILENAME ;
-    }
-    fname[ fnameLen ] = '\0';
-   
-    /* If server cant open file then inform client of this and terminate */
-    if((fp = fopen(fname, "r")) == NULL) /*cant open file*/
-        fail = BADFILENAME;
-
-    tmp = htons(fail);
-    if((writen(newsd,(char *)&tmp,sizeof(tmp))) < 0) {
-        perror("Write error" );
-        exit(0);   
-    }
-    if(fail == BADFILENAME) {
-        printf("server cant open file\n");
-        close(newsd);
-        exit(0);
-    }
-    printf("server: filename is %s\n",fname);
-  
-    req = 0;
-    if ( readn(newsd,(char *)&req,sizeof(req)) < 0) {
-        perror( "Read error" );
-        exit(0);
-    }
-    printf("server: start transfer command, %d, received\n", ntohs(req));
-
-   
-    /*Server gets filesize and calculates the number of blocks of 
-      size = maxsize it will take to transfer the file. Also calculate
-      number of bytes in the last partially filled block if any.  Send
-      this info to client, receiving acks */
-
-    printf("server: starting transfer\n");
-    fsize = 0;ack = 0;   
-    while ((c = getc(fp)) != EOF) {fsize++;}
-    num_blks = fsize / MAXSIZE; 
-    num_blks1 = htons(num_blks);
-    num_last_blk = fsize % MAXSIZE; 
-    num_last_blk1 = htons(num_last_blk);
-    if((writen(newsd,(char *)&num_blks1,sizeof(num_blks1))) < 0) {
-        perror( "Write error" );
-        exit(0);
-    }
-    printf("server: told client there are %d blocks\n", num_blks);  
-    if((readn(newsd,(char *)&ack,sizeof(ack))) < 0) {
-        perror("ack read error " );
-        exit(0); 
-    }          
-    if (ntohs(ack) != ACK) {
-        printf("client: ACK not received on file size\n");
-        exit(0);
-    }
-    if((writen(newsd,(char *)&num_last_blk1,sizeof(num_last_blk1))) < 0) {
-        perror( "write error" );
-        exit(0);
-    }
-    printf("server: told client %d bytes in last block\n", num_last_blk);  
-    if( readn(newsd,(char *)&ack,sizeof(ack)) < 0) {
-        perror( "ack read error" );
-        exit(0); 
-    }
-    if (ntohs(ack) != ACK) {
-        printf("server: ACK not received on file size\n");
-        exit(0);
-    }
-    rewind(fp);    
-  
-    /* Actual file transfer starts  block by block*/       
-       
-    for(i= 0; i < num_blks; i ++) { 
-        no_read = fread(out_buf,sizeof(char),MAXSIZE,fp);
-        if (no_read == 0) {
-            printf("server: file read error\n");
-            exit(0);
-        }
-        if (no_read != MAXSIZE) {
-            printf("server: file read error : no_read is less\n");
-            exit(0);
-        }
-        if((writen(newsd,out_buf,MAXSIZE)) < 0) {
-            perror("Write error: sending block" );
-            exit(0);
-        }
-        if((readn(newsd,(char *)&ack,sizeof(ack))) < 0) {
-            perror("ack read  error" );
-            exit(0);
-        }
-        if (ntohs(ack) != ACK) {
-            printf("server: ACK not received for block %d\n",i);
-            exit(0);
-        }
-        printf(" %d...",i);
-    }
-
-    if (num_last_blk > 0) { 
-        printf("%d\n",num_blks);
-        no_read = fread(out_buf,sizeof(char),num_last_blk,fp); 
-        if (no_read == 0) {
-            printf("server: file read error\n");
-            exit(0);
-        }
-        if (no_read != num_last_blk) {
-            printf("server: file read error : no_read is less 2\n");
-            exit(0);
-        }
-        if((writen(newsd,out_buf,num_last_blk)) < 0) {
-            perror("File transfer error" );
-            exit(0);
-        }
-        if((readn(newsd,(char *)&ack,sizeof(ack))) < 0) {
-            perror("ack read  error" );
-            exit(0);
-        }
-        if (ntohs(ack) != ACK) {
-            printf("server: ACK not received last block\n");
-            exit(0);
-        }
-    }
-    else printf("\n");
-                                                  
-    /* FILE TRANSFER ENDS */
-    printf("server: FILE TRANSFER COMPLETE on socket %d\n",newsd);
-    fclose(fp);
-    close(newsd);
-}
-
-/* Child procedure, which actually does the command transfer */
-void doctp(int newsd)
-{       
-    int msg_ok,fail,req;
+void receiveCommand(int newsd)
+{
+    string cmd;
+    while(cmd != "gbye"){
+    	int msg_ok,fail,req;
     int tmp;
     char command[MAXLINE], commandLen = 0;
     
@@ -310,7 +123,7 @@ void doctp(int newsd)
         fail = BADCOMMAND ;
     }
     command[ commandLen ] = '\0';
-   
+    cmd = command;  
     tmp = htons(fail);
     if((writen(newsd,(char *)&tmp,sizeof(tmp))) < 0) {
         perror("Write error" );
@@ -321,114 +134,69 @@ void doctp(int newsd)
         close(newsd);
         exit(0);
     }
-    printf("server: command is %s\n",command);
+    printf("server: command is: %s\n",command);
   
-    /*
-	req = 0;
-    if ( readn(newsd,(char *)&req,sizeof(req)) < 0) {
-        perror( "Read error" );
-        exit(0);
-    }
-    printf("server: start transfer command, %d, received\n", ntohs(req));
-
-   
-    /*Server gets filesize and calculates the number of blocks of 
-      size = maxsize it will take to transfer the file. Also calculate
-      number of bytes in the last partially filled block if any.  Send
-      this info to client, receiving acks 
-
-    printf("server: starting transfer\n");
-    fsize = 0;ack = 0;   
-    while ((c = getc(fp)) != EOF) {fsize++;}
-    num_blks = fsize / MAXSIZE; 
-    num_blks1 = htons(num_blks);
-    num_last_blk = fsize % MAXSIZE; 
-    num_last_blk1 = htons(num_last_blk);
-    if((writen(newsd,(char *)&num_blks1,sizeof(num_blks1))) < 0) {
-        perror( "Write error" );
-        exit(0);
-    }
-    printf("server: told client there are %d blocks\n", num_blks);  
-    if((readn(newsd,(char *)&ack,sizeof(ack))) < 0) {
-        perror("ack read error " );
-        exit(0); 
-    }          
-    if (ntohs(ack) != ACK) {
-        printf("client: ACK not received on file size\n");
-        exit(0);
-    }
-    if((writen(newsd,(char *)&num_last_blk1,sizeof(num_last_blk1))) < 0) {
-        perror( "write error" );
-        exit(0);
-    }
-    printf("server: told client %d bytes in last block\n", num_last_blk);  
-    if( readn(newsd,(char *)&ack,sizeof(ack)) < 0) {
-        perror( "ack read error" );
-        exit(0); 
-    }
-    if (ntohs(ack) != ACK) {
-        printf("server: ACK not received on file size\n");
-        exit(0);
-    }
-    rewind(fp);    
-  
-    /* Actual file transfer starts  block by block       
-       
-    for(i= 0; i < num_blks; i ++) { 
-        no_read = fread(out_buf,sizeof(char),MAXSIZE,fp);
-        if (no_read == 0) {
-            printf("server: file read error\n");
-            exit(0);
-        }
-        if (no_read != MAXSIZE) {
-            printf("server: file read error : no_read is less\n");
-            exit(0);
-        }
-        if((writen(newsd,out_buf,MAXSIZE)) < 0) {
-            perror("Write error: sending block" );
-            exit(0);
-        }
-        if((readn(newsd,(char *)&ack,sizeof(ack))) < 0) {
-            perror("ack read  error" );
-            exit(0);
-        }
-        if (ntohs(ack) != ACK) {
-            printf("server: ACK not received for block %d\n",i);
-            exit(0);
-        }
-        printf(" %d...",i);
-    }
-
-    if (num_last_blk > 0) { 
-        printf("%d\n",num_blks);
-        no_read = fread(out_buf,sizeof(char),num_last_blk,fp); 
-        if (no_read == 0) {
-            printf("server: file read error\n");
-            exit(0);
-        }
-        if (no_read != num_last_blk) {
-            printf("server: file read error : no_read is less 2\n");
-            exit(0);
-        }
-        if((writen(newsd,out_buf,num_last_blk)) < 0) {
-            perror("File transfer error" );
-            exit(0);
-        }
-        if((readn(newsd,(char *)&ack,sizeof(ack))) < 0) {
-            perror("ack read  error" );
-            exit(0);
-        }
-        if (ntohs(ack) != ACK) {
-            printf("server: ACK not received last block\n");
-            exit(0);
-        }
-    }
-    else printf("\n");
-                                                  
-    /* FILE TRANSFER ENDS 
-	*/
     printf("server: Command TRANSFER COMPLETE on socket %d\n",newsd);
+    }
     close(newsd);
+}
+
+void giveCommand(int newsd)
+{
+	string userCommand;
+    int getcommand,msg,msg_2,len;
+	
+	getUserInput(userCommand);
+    
+	while(userCommand != "shutdown"){
+        len = userCommand.size();
+
+	    getcommand = htons(REQUESTCOMMAND);
+        printf("client: sending command request to ftp server\n");
+        if((writen(newsd,(char *)&getcommand,sizeof(getcommand))) < 0) {
+            printf("client: write  error :%s\n",  strerror( errno )); 
+            exit(0);
+        } 
+
+        /* want for go-ahead from server */
+        msg = 0;  
+        if((readn(newsd,(char *)&msg,sizeof(msg)))< 0) {
+            printf("client: read  error :%s\n",  strerror( errno )); 
+         exit(0); 
+        }
+        msg = ntohs(msg);   
+        if (msg==COMMANDNOTSUPPORTED) {
+            printf("client: server refused command. goodbye\n");
+            exit(0);
+        } else
+            printf("client: server replied %d, command supported\n", msg);
+
+        /* send file name to server */
+        printf("client: sending command\n");
+        if ((writen(newsd, (char *)userCommand.c_str(),len))< 0) {
+            printf("client: write  error :%s\n",  strerror( errno )); 
+            exit(0);
+        }
+        /* see if server replied that file name is OK */
+        msg_2 = 0;
+        if (readn(newsd, (char *)&msg_2, sizeof(msg_2)) < 0) {
+            printf("client: read  error :%s\n",  strerror( errno )); 
+            exit(0); 
+        }   
+        msg_2 = ntohs(msg_2);
+        if (msg_2 == BADCOMMAND) {       
+            printf("client: server reported bad command. goodbye.\n");
+            exit(0);
+        } else
+            printf("client: server replied %d, filename OK\n",msg_2);
+    
+
+        /*File transfer ends. client terminates after  closing all its files
+          and sockets*/ 
+        printf("client: Command TRANSFER COMPLETE\n");
+        getUserInput(userCommand);
+    }
+    //close(newsd);
 }
 
 /*
@@ -460,4 +228,10 @@ int writen(int sd,char *ptr,int size)
         ptr += no_written;
     }
     return(size - no_left);
+}
+
+void getUserInput(string & userCommand)
+{
+    cout << "~: ";
+    getline(cin, userCommand);
 }
